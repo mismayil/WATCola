c88d91fef8a8dacec6fa81472155336125c41c45
Rasul <rrasulza@yahoo.com>
2015-12-03 00:31:05 -0500

    Parent_Bank_MPRNG_Conf

---
+++ bank.cc
@@ -0,0 +1,44 @@
+#include "bank.h"
+
+
+// Bank constructor
+Bank::Bank( unsigned int numStudents ) : numStudents(numStudents){
+
+	// each student’s account initially starts with a balance of $0.
+	accounts = new unsigned int[numStudents];
+	for (int i = 0; i < numStudents; i++){
+		accounts[i] = 0;
+	}
+
+	couriers = new uCondition[numStudents];
+}
+
+
+// Bank destructor
+Bank::~Bank(){
+	delete[] accounts;
+	delete[] couriers;
+}	
+
+
+// endows gifts to a specific student 
+void Bank::deposit( unsigned int id, unsigned int amount ){
+	accounts[id] += amount;
+	couriers[id].signal();
+}
+
+// transfers money for a specific student.
+void Bank::withdraw( unsigned int id, unsigned int amount ){
+
+	// courier waits until enough money has been deposited
+	while(accounts[id] < amount){
+		couriers[id].wait();
+
+		// call the next courier to do withdraw,
+		// if there is one on the bench queue
+		if(!couriers.empty()){	
+			couriers[id].signal();
+		}
+	}
+	accounts[id] -= amount;
+}
---
+++ bank.h
@@ -0,0 +1,17 @@
+#ifndef __BANK_H__
+#define __BANK_H__
+
+_Monitor Bank {
+	unsigned int numStudents;
+	unsigned int *accounts;
+	uCondition *courier;
+
+  public:
+    Bank( unsigned int numStudents );
+    ~Bank();
+    void deposit( unsigned int id, unsigned int amount );
+    void withdraw( unsigned int id, unsigned int amount );
+};
+
+
+#endif
