12a2bd4fc3d680af820f5a146700c0c26977fed6
Rasul <rrasulza@yahoo.com>
2015-12-06 19:09:32 -0500

    RASUL

--- student.cc
+++ student.cc
@@ -49,18 +49,18 @@ void Student::main(){
     	for(;;){
     		try{
     			_Enable{
-                    // _Select( g_card ){
+                    _Select( g_card ){
 
-                    //     // pay with GiftCard and reset 
-                    //     v_machine->buy(flavour, *g_card());
+                        // pay with GiftCard and reset 
+                        v_machine->buy(flavour, *g_card());
 
-                    //     // print GiftCard balance message
-                    //     prt.print(Printer::Student, (int) id, 'G', g_card()->getBalance());   
-                    //     g_card.reset();
-                    //     break;
-                    // }
-                    // or 
-                    // _Select( w_card ){
+                        // print GiftCard balance message
+                        prt.print(Printer::Student, (int) id, 'G', g_card()->getBalance());   
+                        g_card.reset();
+                        break;
+                    }
+                    or 
+                    _Select( w_card ){
 
                         // pay with WATCard
                         v_machine->buy(flavour, *w_card());
@@ -101,7 +101,7 @@ void Student::main(){
 	}
 
     delete w_card(); 
-    // delete g_card();
+    delete g_card();
 
 	//print Finish message
 	prt.print(Printer::Student, (int) id, 'F');
