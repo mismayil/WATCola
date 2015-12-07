55a95bde4bc542030f0113e70802a65656d90382
Rasul <rrasulza@yahoo.com>
2015-12-06 23:50:18 -0500

    RASUL

--- watcardOffice.cc
+++ watcardOffice.cc
@@ -128,10 +128,12 @@ void WATCardOffice::Courier::main(){
 
         // 1 in 6 chance a courier loses a student’s WATCard
 		if(mprnGen(5) == 0){
-			
+			// the exception WATCardOffice::Lost is inserted into the future
+			j->result.exception(new Lost);
+
 			// the current WATCard is deleted
 			if(j->args.w_card != NULL){
-			   // delete j->args.w_card;
+				// delete j->args.w_card;
 				j->args.w_card = NULL;
 			}
 		}
