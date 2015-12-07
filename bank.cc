5e087702dc883068418fd2c098ebadb7ee43bcda
Rasul <rrasulza@yahoo.com>
2015-12-06 19:22:01 -0500

    RASUL

--- bank.cc
+++ bank.cc
@@ -10,36 +10,36 @@ Bank::Bank( unsigned int numStudents ) : numStudents(numStudents){
 		accounts[i] = 0;
 	}
 
-	couriers = new uCondition[numStudents];
+	// couriers = new uCondition[numStudents];
 }
 
 
 // Bank destructor
 Bank::~Bank(){
 	delete[] accounts;
-	delete[] couriers;
+	// delete[] couriers;
 }	
 
 
 // endows gifts to a specific student 
 void Bank::deposit( unsigned int id, unsigned int amount ){
 	accounts[id] += amount;
-	couriers[id].signal();
+	// couriers[id].signal();
 }
 
 // transfers money for a specific student.
 void Bank::withdraw( unsigned int id, unsigned int amount ){
 
 	// courier waits until enough money has been deposited
-	while(accounts[id] < amount){
-		couriers[id].wait();
+	// while(accounts[id] < amount){
+		// couriers[id].wait();
 
 		// call the next courier to do withdraw,
 		// if there is one on the bench queue
 		// if(!couriers[id].empty()){	
 		// 	couriers[id].signal();
 		// }
-	}
+	// }
 	accounts[id] -= amount;
 }
 
